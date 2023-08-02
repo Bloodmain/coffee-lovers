2edc950ed26809975fafff79704e8c81f6c37d8f
Dunaev Kirill <60296337+Bloodmain@users.noreply.github.com>
2023-08-02 19:53:06 +0500

    Spring init

---
+++ src/main/java/ru/dnk/coffeelovers/CoffeeLoversApplication.java
@@ -0,0 +1,13 @@
+package ru.dnk.coffeelovers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeLoversApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeLoversApplication.class, args);
+    }
+
+}
